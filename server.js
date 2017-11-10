3b56980fce29190f2230a426a3e72659b321572e
@racascou <rafaelcastrocouto@gmail.com>
2017-11-10 15:18:24 -0200

    0.1.3 - Client Auto Load

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-var http = require('http');
-var read = require('node-read');
+var fs      = require('fs');
+var http    = require('http');
+var read    = require('node-read');
 var cheerio = require('cheerio');
-var static = require('serve-static')(__dirname);
-var host = process.env.HOST;
-var port = process.env.PORT || 5000;
-var fs = require('fs');
+var static  = require('serve-static')(__dirname);
+var host    = process.env.HOST;
+var port    = process.env.PORT || 5000;
 
 require.extensions['.html'] = function(module, filename) {
   module.exports = fs.readFileSync(filename, 'utf8');
 }
 
 var header = require("./header.html");
-var menu = require("./menu.html");
+var menu   = require("./menu.html");
 var script = require("./script.html");
+
 var body = menu;
 
 var extractList = function(url, selector, cb) {
@@ -32,7 +33,7 @@ var createServer = function() {
     static(request, response, function onNext(err) {
       response.setHeader('Content-Type', 'text/html; charset=UTF-8');
       response.statusCode = 200;
-      response.end(header + body);
+      response.end(header + body + script);
     });
   }).listen(port, host);
 }
@@ -42,7 +43,7 @@ var update = function() {
   extractList('https://www.youtube.com/user/canalpoenaroda/videos', '.channels-content-item', function(youtube) {
     body += '<div id="yt">' + youtube + '</div>';
     extractList('http://poenaroda.com.br', '.col-sm-6', function(superpride) {
-      body += '<div id="news"><h2>Novidades</h2>' + superpride + '</div>' + script;
+      body += '<div id="news"><h2>Novidades</h2>' + superpride + '</div>';
     });
   });
 }
